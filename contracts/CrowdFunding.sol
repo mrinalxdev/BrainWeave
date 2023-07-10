e57e2c31426651c6dc90c263a61b95bdf28e0127
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-10 21:12:35 +0530

    Create campaingn Function added

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -5,7 +5,7 @@ contract CrowdFunding {
     struct Campaign {
         address owner;
         string title;
-        string  description;
+        string description;
         uint256 target;
         uint256 deadline;
         uint256 amountCollected;
@@ -14,5 +14,33 @@ contract CrowdFunding {
     }
 
     mapping(uint256 => Campaign) public campaigns;
-    
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(
+            campaign.deadline < block.timestamp,
+            "The Deadline should be in the future"
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+
+
+        numberOfCampaigns ++ ;
+
+        return numberOfCampaigns - 1;
+    }
 }
