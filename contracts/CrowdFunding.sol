4ed23e8c831deb5c559a55f12b8c0fe4ca453395
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-10 21:29:12 +0530

    Done : Smart Contract Logic

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -38,9 +38,39 @@ contract CrowdFunding {
         campaign.deadline = _deadline;
         campaign.amountCollected = 0;
 
-
-        numberOfCampaigns ++ ;
+        numberOfCampaigns++;
 
         return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
     }
 }
