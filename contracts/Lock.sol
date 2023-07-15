db158ad0041a126e86f06579c7a8af4ff5d4f09e
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-16 04:28:22 +0530

    Main : Contains Main Solidity File

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    struct Memo {
+        string name; 
+        string message; 
+        uint timestamp;
+        address from;
+    }
 
-    event Withdrawal(uint amount, uint when);
+    Memo[] memos; 
+    address payable owner;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
+    constructor(){
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function buyShake(string calldata name, string calldata message) external payable{
+        require(msg.value > 0, "Amount should be above 0"); 
+        owner.transfer(msg.value);
+        memos.push(Memo(name, message,block.timestamp,msg.sender));
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
     }
 }
