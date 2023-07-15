4efa2fe08cf331d5910464530e94a8fce5077c9a
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-16 04:30:45 +0530

    Contains : Files related to deploying of smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,8 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
 
   const lock = await hre.ethers.deployContract("Lock");
 
@@ -19,9 +13,8 @@ async function main() {
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+// 0x8f789106ef402B69D81589Ec791D0579F61bb59F
